04a403bddfb93862c4eaef49d86312f4e703cda8
khaymanii <kennethakpo61@gmail.com>
2025-05-12 08:24:34 +0100

    fixed

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,7 @@
 const Spinner = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="h-12 w-12 border-4 border-black border-t-transparent border-solid rounded-full animate-spin"></div>
+      <div className="h-10 w-10 border-4 border-black border-t-transparent border-solid rounded-full animate-spin"></div>
     </div>
   );
 };
