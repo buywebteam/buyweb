bec83b950522b3aa239804152f7d5d2997db5fa4
buywebteam <buywebteam@gmail.com>
2025-06-29 11:08:39 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,10 +6,10 @@ function Hero() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-40">
         <div className="flex flex-col justify-center items-center">
           <h1 className="font-bold lg:text-5xl text-3xl text-center lg:w-[700px]">
-            Buy Beautiful Ready-Made Websites at a Cheaper Rate
+            Buy Beautiful Ready-Made Websites and Templates at a Cheaper Rate
           </h1>
           <p className="mt-8 lg:text-2xl text-center lg:w-[900px]">
-            Discover a wide range of beautifully crafted, ready-made websites
+            Discover a wide range of beautifully crafted, ready-made websites and website templates
             tailored for your business—at a price that fits your budget
           </p>
           <div className="mt-8 flex lg:flex-row md:flex-row flex-col justify-center items-center w-full gap-4">
